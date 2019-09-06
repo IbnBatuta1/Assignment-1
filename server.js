7121bec2c956c356f8f8e00d6a7e5d53287c1506
IbnBatuta1 <molhamlucy@gmail.com>
2019-09-06 19:46:15 -0400

    Bootcamp Assignment#1

--- server.js
+++ server.js
@@ -1,51 +1,34 @@
-var http = require('http'), 
-    fs = require('fs'), 
-    url = require('url'),
-    port = 8080;
+var http = require('http'),
+  fs = require('fs'),
+  url = require('url'),
+  port = 8080;
 
 /* Global variables */
 var listingData, server;
 
-var requestHandler = function(request, response) {
+var requestHandler = function (request, response) {
   var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+	if (request.method === "GET" && parsedUrl.pathname === "/listings") {
+    //Send listingData in the JSON format as a respone if a GET is sent to the '/listings' path.
+		response.statusCode = 200;
+		response.write(listingData);
+	} else {
+		response.statusCode = 404;
+		response.write('Bad gateway error');
+	}
+		response.end();
 };
 
-fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+fs.readFile('listings.json', 'utf8', function (err, data) {
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-
-    HINT: Read up on JSON parsing Node.js
-   */
-
-    //Check for errors
-  
-
-   //Save the sate in the listingData variable already defined
-  
-
-  //Creates the server
-  
-  //Start the server
-
-
+	listingData = data;
+	// REFERENCING simpleServer.js
+	//Creates the Server
+	server = http.createServer(requestHandler);
+	//Starts the Server
+	server.listen(port, function () {
+		console.log('server listening on: http://localhost:' + port);
+	});
 });
+
